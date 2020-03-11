21e518e63ce962a7b2ecf1a97077302203661a33
James Chen <60947635+jamesnanchen@users.noreply.github.com>
2020-03-11 15:37:28 -0500

    Add files via upload

--- CodeCovearge/src/main/java/cs6367Project/Agent.java
+++ CodeCovearge/src/main/java/cs6367Project/Agent.java
@@ -5,5 +5,17 @@ import java.lang.instrument.Instrumentation;
 public class Agent {
     public static void premain(String agentArgs, Instrumentation inst) {
         inst.addTransformer(new ClassTransformer("org/apache/commons/dbutils/"));
+        inst.addTransformer(new ClassTransformer("org/joda/time/"));
+        inst.addTransformer(new ClassTransformer("org/hashids/"));
+        inst.addTransformer(new ClassTransformer("cz/startnet/utils/pgdiff/"));
+        inst.addTransformer(new ClassTransformer("brickhouse/"));
+        inst.addTransformer(new ClassTransformer("com/jakewharton/byteunits"));
+        inst.addTransformer(new ClassTransformer("com/sampullara/cli"));
+        inst.addTransformer(new ClassTransformer("eu/danieldk/dictomaton"));
+        inst.addTransformer(new ClassTransformer("com/github/vbauer/caesar/"));
+
+        inst.addTransformer(new ClassTransformer("org/trendafilov/confucius"));
+
+        inst.addTransformer(new ClassTransformer("io/gsonfire/"));
     }
 }
