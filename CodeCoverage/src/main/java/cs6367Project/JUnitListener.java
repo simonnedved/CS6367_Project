69eea54149a967c4f7481460032ef614bbb33d2c
James Chen <60947635+jamesnanchen@users.noreply.github.com>
2020-04-09 10:41:49 -0500

    Add files via upload

--- CodeCoverage/src/main/java/cs6367Project/JUnitListener.java
+++ CodeCoverage/src/main/java/cs6367Project/JUnitListener.java
@@ -11,6 +11,26 @@ import java.util.LinkedHashSet;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.Failure;
+
+import org.junit.platform.commons.meta.API;
+import org.junit.platform.engine.TestExecutionResult;
+import org.junit.platform.engine.TestExecutionResult.Status;
+import org.junit.platform.engine.reporting.ReportEntry;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.junit.platform.launcher;
+
 
 public class JUnitListener extends RunListener {
 
@@ -19,7 +39,46 @@ public class JUnitListener extends RunListener {
         super.testRunStarted(description);
         CoverageCollection.testSuite = new HashMap<String, HashMap<String, LinkedHashSet<Integer>>>();
     }
+    @Override
+    public void testPlanExecutionStarted(TestPlan testPlan) throws Exception{
+        super.testPlanExecutionStarted(testPlan);
+        CoverageCollection.testSuite = new HashMap<String, HashMap<String, LinkedHashSet<Integer>>>();
+    }
 
+    @Override
+    public void testPlanExecutionFinished(TestPlan testPlan) throws Exception{
+        super.testRunFinished(testPlan);
+        String dir = "coverageLog";
+        String logPath = dir + File.separator + "stmt-cov.txt";
+        try {
+            File directory = new File(dir);
+            if (! directory.exists()){
+                directory.mkdir();
+            }
+
+            File file = new File(logPath);
+            if (!file.exists())
+                file.createNewFile();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+            StringBuilder sb = new StringBuilder();
+            for (String testName : CoverageCollection.testSuite.keySet()) {
+                sb.append(testName + "\n");
+                HashMap<String, LinkedHashSet<Integer>> testCase = CoverageCollection.testSuite.get(testName);
+
+                for (String className : testCase.keySet()) {
+                    for(int i : testCase.get(className)){
+                        sb.append(className + ":" + i + "\n");
+                    }
+                }
+            }
+            bw.write(sb.toString());
+            bw.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void testRunFinished(Result result) throws Exception {
         super.testRunFinished(result);
@@ -60,13 +119,35 @@ public class JUnitListener extends RunListener {
         super.testStarted(description);
         CoverageCollection.testName = "[TEST] " + description.getClassName() + ":" + description.getMethodName();
         CoverageCollection.testCase = new HashMap<String, LinkedHashSet<Integer>>();
+        CoverageCollection.testSuite.put(CoverageCollection.testName,CoverageCollection.testCase);
+    }
+
+    @Override
+    public void executionStarted(TestIdentifier testIdentifier) throws Exception{
+        super.testStarted(testIdentifier);
+        CoverageCollection.testName = "[TEST] " + testIdentifier.getDisplayName() + ":" + testIdentifier.getUniqueId();
+        CoverageCollection.testCase = new HashMap<String, LinkedHashSet<Integer>>();
+        CoverageCollection.testSuite.put(CoverageCollection.testName,CoverageCollection.testCase);
+    }
+    @Override
+    public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) throws Exception{
+        super.executionFinished(testIdentifier,testExecutionResult);
     }
 
     @Override
     public void testFinished(Description description) throws Exception {
-        CoverageCollection.testSuite.put(CoverageCollection.testName,CoverageCollection.testCase);
         super.testFinished(description);
+    }
 
+    @Override
+    public void executionSkipped(TestIdentifier testIdentifier, String reason) throws Exception{
+        super.executionSkipped(testIdentifier,reason);
+    }
+
+
+    @Override
+    public void testIgnored(Description description) throws Exception {
+        super.testIgnored(description);
     }
 
 }
